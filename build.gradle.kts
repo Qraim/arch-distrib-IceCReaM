7ec21c02c988caa3c8a084ee7c90954b0607a499
qraim <quentin.raimbault007@gmail.com>
2024-11-22 10:47:10 +0100

    Graddle

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.apache.thrift:libthrift:0.13.0")
+    implementation("org.slf4j:slf4j-api:1.7.36")
+    implementation("ch.qos.logback:logback-classic:1.2.11")
+    testImplementation("org.mockito:mockito-core:2.1.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
